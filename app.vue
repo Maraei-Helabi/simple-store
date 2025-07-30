b2eecf4ba2cf392ba9e6d2eb524d3209b7190da6
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2025-07-31 00:02:36 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+import { defineNuxtPlugin } from '#app'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+  })
+  nuxtApp.vueApp.use(vuetify)
+})
+</script>
