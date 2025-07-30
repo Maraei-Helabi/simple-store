52ead22496356ee460b792e43774de137d527fe5
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2025-07-31 00:06:56 +0300

    style: add vuetify lib

--- app.vue
+++ app.vue
@@ -4,19 +4,3 @@
     <NuxtWelcome />
   </div>
 </template>
-
-<script setup>
-import { defineNuxtPlugin } from '#app'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import 'vuetify/styles'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify = createVuetify({
-    components,
-    directives,
-  })
-  nuxtApp.vueApp.use(vuetify)
-})
-</script>
