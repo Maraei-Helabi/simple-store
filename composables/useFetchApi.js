db53f42df8dd623ffa141c8077103595eebe72f8
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2025-08-02 03:20:58 +0300

    fix: useFetchApi no retraver date

--- composables/useFetchApi.js
+++ composables/useFetchApi.js
@@ -1,8 +1,12 @@
-export const useFetchApi =(request, options) => {
-    const config = useRuntimeConfig();
-    
-    return useFetch(request, {
+export const useFetchApi = (request, options = {}) => {
+  const config = useRuntimeConfig();
+
+  const response = useFetch(request, {
     baseURL: config.public.baseURL,
     ...options
-    })
-}
+  });
+
+   console.log("🔥 استجابة الـ API:", response);
+
+  return response
+};
