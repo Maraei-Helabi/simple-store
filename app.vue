02bc8cfa42d3ab457ad906268ee9060eb1760700
Mareai_Bin_Helabi <mrai.hlabi7@gmail.com>
2025-08-02 03:18:47 +0300

    fix: toast config

--- app.vue
+++ app.vue
@@ -1,3 +1,8 @@
 <template>
   <NuxtLayout />
+
 </template>
+
+<script setup>
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,47 @@
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
-    plugins: [
-    '~/plugins/vuetify'
+  plugins: [
+    '~/plugins/vuetify',
+    '~/plugins/vue-toastification'
   ],
-    modules: [
-    'pinia/nuxt',
+  modules: [
+    '@pinia/nuxt',
+    // إضافة vuetify إلى إعدادات Vite
+    async (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
   ],
-    css: [
+  css: [
     '@mdi/font/css/materialdesignicons.min.css'
   ],
+  runtimeConfig: {
+    public: {
+      baseURL: process.env.NUXT_PUBLIC_BASE_URL,
+      userId: process.env.NUXT_PUBLIC_USER_ID
+    }
+  },
+  ssr: false,
+  app: {
+    head: {
+      htmlAttrs: {
+        dir: "rtl",
+      },
+    },
+  },
+  build: {
+    transpile: ["vuetify"]
+  },
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
